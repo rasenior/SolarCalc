c521d64adf0df18eedaa646bf21f80af0b2f5c02
rasenior <rebecca.a.senior@gmail.com>
2017-06-27 11:44:45 +0100

    Removed call to function from ConvertDateTime repo.
    Signed-off-by: rasenior <rebecca.a.senior@gmail.com>

--- R/solar_calc.R
+++ R/solar_calc.R
@@ -1,14 +1,14 @@
 #' solar_calc
 #'
-#' Calculate the time of sunrise and sunset based on date and location. 
-#' @param latitude Latitude in decimal degrees. 
+#' Calculate the time of sunrise and sunset based on date and location.
+#' @param latitude Latitude in decimal degrees.
 #' @param longitude Longitude in decimal degrees.
 #' @param excel_date Date in MS Excel format. See https://github.com/rasenior/ConvertDateTime for functions to convert dates into Excel format.
 #' @param time_zone Time zone (numeric), taking account of Daylight saving time where applicable.
 #' @param return_var Variables to return: 'both', 'sunrise' or 'sunset'. Defaults to 'both'.
 #' @return Either single sunset or sunrise value expressed as minutes of the day, or a dataframe with both variables combined.
 #' @examples
-#' solar_calc(latitude = 53.6955847, longitude = -2.0015868, excel_date = norm2excel(norm_date = '27/06/2017'), time_zone = 1, return_var = 'both')
+#' solar_calc(latitude = 53.6955847, longitude = -2.0015868, excel_date = 42913, time_zone = 1, return_var = 'both')
 solar_calc <- function(latitude, longitude, excel_date, time_zone, return_var = "both") {
     jd <- julian_day(excel_date, time_zone)
     jc <- julian_century(jd)
@@ -30,7 +30,7 @@ solar_calc <- function(latitude, longitude, excel_date, time_zone, return_var = 
     sn <- solar_noon(longitude, et, time_zone)
     sunrise <- sn - (has * 4)
     sunset <- sn + (has * 4)
-    
+
     # Define which results to return
     if (tolower(return_var) == "both") {
         result <- dataframe(sunrise = sunrise, sunset = sunset)
@@ -39,6 +39,6 @@ solar_calc <- function(latitude, longitude, excel_date, time_zone, return_var = 
     } else if (tolower(return_var) == "sunset") {
         result <- sunset
     }
-    
+
     return(result)
 }
